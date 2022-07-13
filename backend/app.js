a34db349b7b4ae65410c2524e1ee2218690e6284
hoanglechau <hoangchau612@gmail.com>
2022-07-13 15:14:37 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,56 @@
 const express = require('express');
+// listen to port 3000
+const { PORT = 3000 } = process.env;
 const mongoose = require('mongoose');
-const helmet = require('helmet');
+const cors = require('cors');
+const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middleware/logger');
+require('dotenv').config();
+
 const routes = require('./routes');
 
-const { PORT = 3000 } = process.env;
+console.log(process.env.NODE_ENV);
+
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/aroundb');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(helmet());
-app.use(routes);
+app.use(cors());
 
 app.use((req, res, next) => {
-  req.user = {
-    _id: '5d8b8592978f8bd833ca8133',
-  };
-
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://api.hoanglechau.students.nomoreparties.sbs',
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   next();
 });
 
-app.listen(PORT, () => console.log('ok'));
+app.use(cors());
+app.options('*', cors()); // Enable requests for all routes
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server will crash now');
+  }, 0);
+});
+app.use(routes);
+app.use(errorLogger);
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ message: 'An error occurred on the server' });
+});
+
+app.listen(PORT, () => {
+  // if everything works fine, the console will show which port the application is listening to
+  console.log(`App listening at port ${PORT}`);
+});
