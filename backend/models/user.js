164d64d7d3e727ee17aa3e493ec4eb6fbf1b4f20
hoanglechau <hoangchau612@gmail.com>
2022-07-13 14:34:53 +0700

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,27 +1,66 @@
 const mongoose = require('mongoose');
-const { urlRegExp } = require('../utils/regex');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'The "name" field must be filled in'],
-    minlength: [2, 'The minimum length of the "name" field is 2'],
-    maxlength: [30, 'The maximum length of the "name" field is 30'],
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+    default: 'Hoang Le Chau',
   },
   about: {
     type: String,
-    required: [true, 'The "about" field must be filled in'],
-    minlength: [2, 'The minimum length of the "about" field is 2'],
-    maxlength: [30, 'The maximum length of the "about" field is 30'],
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+    default: 'Software Engineer',
   },
   avatar: {
     type: String,
-    required: [true, 'The "avatar" field must be filled in'],
+    required: true,
+    default: 'https://pictures.s3.yandex.net/resources/avatar_1604080799.jpg',
     validate: {
-      validator: (v) => urlRegExp.test(v),
-      message: 'The "avatar" field must be a valid URL',
+      validator: validator.isURL,
+      message: 'The "avatar" must be a valid url',
     },
   },
-}, { versionKey: false });
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: 'The "email" must be a valid email address',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password,
+) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Incorrect email or password'));
+      }
+
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error('Incorrect email or password'));
+        }
+
+        return user; // now user is available
+      });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
