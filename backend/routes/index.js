6e590e918cfc0855ff01bb90c265cd97b54d3fa9
hoanglechau <hoangchau612@gmail.com>
2022-07-13 14:35:02 +0700

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,12 +2,21 @@ const router = require('express').Router();
 
 const userRouter = require('./users');
 const cardRouter = require('./cards');
+const NotFoundError = require('../errors/not-found-error');
 
+const auth = require('../middleware/auth');
+const { createUser, login } = require('../controllers/users');
+const { validateLogin, validateUser } = require('../middleware/validation');
+
+router.post('/signin', validateLogin, login);
+router.post('/signup', validateUser, createUser);
+
+router.use(auth);
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
 
-router.use((req, res) => {
-  res.status(404).send({ message: 'Requested resource not found' });
+router.use((req, res, next) => {
+  next(new NotFoundError('Requested resource not found'));
 });
 
 module.exports = router;
