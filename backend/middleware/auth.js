ed476df137ea25345df539198c839e949f53fccc
hoanglechau <hoangchau612@gmail.com>
2022-07-19 12:25:47 +0700

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,22 +1,24 @@
 const jwt = require('jsonwebtoken');
 
-const { JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 const UnauthorizedError = require('../errors/unauthorized-error');
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
-  console.log(authorization);
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return next(new UnauthorizedError('Authorization Required'));
   }
 
   const token = authorization.replace('Bearer ', '');
-  console.log(token);
+
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(
+      token,
+      NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret-key',
+    );
   } catch (err) {
     return next(new UnauthorizedError('Authorization Required'));
   }
