0a41e7ad531a7cca70fd7ee4c244e37aec1c7d9f
hoanglechau <hoangchau612@gmail.com>
2022-07-19 13:56:29 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -18,19 +18,32 @@ mongoose.connect('mongodb://localhost:27017/aroundb');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 app.use((req, res, next) => {
-  res.header(
-    'Access-Control-Allow-Origin',
+  const allowedCors = [
     'https://api.hoanglechau.students.nomoredomainssbs.ru',
-  );
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  );
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  next();
+    'https://www.hoanglechau.students.nomoredomainssbs.ru',
+    'https://hoanglechau.students.nomoredomainssbs.ru',
+    'localhost:3000',
+  ];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  res.header('Access-Control-Allow-Origin', '*');
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
 });
 
 app.use(cors());
